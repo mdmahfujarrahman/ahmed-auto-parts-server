605d2377cb05d124a31215b71eb23be4713f1b0b
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-05-24 23:12:56 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
 
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS_SECRET}@cluster0.2nhyc.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverApi: ServerApiVersion.v1,
+    });
 
+
+async function run(){
+    
+    
+    
+    try{
+
+        await client.connect();
+        const partsCollection = client.db("ahmed_parts").collection("parts");
+
+        app.get('/parts', async (req, res) => {
+            const parts = await partsCollection.find().toArray()
+            res.send(parts)
+        })
+        
+        
+
+
+    }
+    finally{
+
+    }
+
+}
+
+
+run().catch(console.dir)
+
+
+//Default router
 app.get('/', (req, res) => {
     res.send('Welcome to Ahmed Auto parts Server')
 })

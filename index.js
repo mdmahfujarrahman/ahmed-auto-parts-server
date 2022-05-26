783087e51508b81eccb08fc6be5aa29ed4c5467c
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-05-26 18:42:30 +0600

    parts detsils updated api create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
@@ -17,6 +17,27 @@ app.use(express.json());
             serverApi: ServerApiVersion.v1,
     });
 
+//Verify token
+
+function verifyToken(req, res, next) {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders) {
+        return res.status(401).send({ message: "unAuthorized access" });
+    }
+    const token = authHeaders.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+
+        if(err){
+            return res.status(403).send({ message: "Forbidden Access"})
+        }
+        res.decoded = decoded
+        next()
+    });
+    
+    
+}   
+
+
 
 async function run(){
     
@@ -26,15 +47,88 @@ async function run(){
 
         await client.connect();
         const partsCollection = client.db("ahmed_parts").collection("parts");
+        const userCollection = client.db("ahmed_parts").collection("users");
+        const orderCollection = client.db("ahmed_parts").collection("orders");
         const reviewsCollection = client
             .db("ahmed_parts")
             .collection("reviews");
+
+        //all user
+        app.get("/users", async (req, res) => {
+            const users = await userCollection.find().toArray()
+            res.send(users)
+        })
+
+
+        // users added
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const results = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1d" }
+            );
+
+            res.send({ results, token });
+        });
+
+
+
 
         //all parts
         app.get('/parts', async (req, res) => {
             const parts = await partsCollection.find().toArray()
             res.send(parts)
         })
+
+        //single parts
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const parts = await partsCollection.findOne(query);
+            res.send(parts)
+        })
+
+       //parts details update
+       app.put('/parts/:id', verifyToken, async (req, res) => {
+           const id = req.params.id;
+           const updateDetails =req.body
+           const filter = { _id: ObjectId(id) };
+           const updateDoc = {
+               $set: {
+                   quantity: updateDetails.quantity,
+               },
+           };
+           const results = await partsCollection.updateOne(filter, updateDoc);
+           res.send(results)
+       })
+
+
+        //Orders
+
+        app.post('/order',  async (req, res) => {
+            const orders = req.body;
+            const query = {partsName: orders.partsName, user: orders.user};
+            const exists = await orderCollection.findOne(query);
+            if(exists) {
+                return res.send({success: false, order: exists})
+            }
+            const results = await orderCollection.insertOne(orders)
+            res.send({success: true, results})
+        })
+
+
 
 
         //all reviews
@@ -43,8 +137,6 @@ async function run(){
             res.send(reviews)
         })
         
-        
-
 
     }
     finally{
